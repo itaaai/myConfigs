f20b8c20e34dcc44110e2175f5c05aa644261d71
Itai Masuari <imasuari@habana.ai>
2025-11-09 14:00:44 +0200

    [nvim] add tab/split/resize keybindss

--- nvim/lua/config/keybinds.lua
+++ nvim/lua/config/keybinds.lua
@@ -8,3 +8,18 @@ end
 
 
 nmap("<leader>ex", "<Cmd>Ex<CR>")
+
+nmap("<leader>-", "<Cmd>split<CR>")
+nmap("<leader>|", "<Cmd>vsplit<CR>")
+nmap("<leader>q", "<C-w>c")
+
+nmap("<leader>t", ":tabnew<CR>");
+nmap("<leader>T", ":tabclose<CR>");
+nmap("<A-S-h>", ":tabprevious<CR>");
+nmap("<A-S-l>", ":tabnext<CR>");
+
+-- Split resizing with Alt + arrow keys (allows continuous pressing)
+nmap("<A-Left>", ":vertical resize -2<CR>");
+nmap("<A-Right>", ":vertical resize +2<CR>");
+nmap("<A-Up>", ":resize -2<CR>");
+nmap("<A-Down>", ":resize +2<CR>");
